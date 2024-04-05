5053f511e327cafe47c788fb2a070787fe855444
Arthur Miranda <119464045+art5hur@users.noreply.github.com>
2024-04-05 11:42:52 -0300

    Complementando o MainActivity.kt

--- app/src/main/java/com/fiap/orgs/activity/MainActivity.kt
+++ app/src/main/java/com/fiap/orgs/activity/MainActivity.kt
@@ -10,7 +10,7 @@ import java.math.BigDecimal
 
 /*
  Classe MainActivity representa a classe principal do app.
- Ela herda de ComponentActivity, que é uma classe fornecida pelo framework Android Jetpack.
+ Ela herda de Activity, que é uma classe fornecida pelo framework Android Jetpack.
  Esta atividade exibe uma lista de produtos em um RecyclerView.
  */
 
@@ -28,13 +28,22 @@ class MainActivity : Activity() {
     O Bundle é usado para armazenar e passar dados entre componentes do Android,
     como atividades (Activity), fragmentos (Fragment), serviços (Service), etc.
     O Bundle é frequentemente utilizado para transferir informações entre diferentes partes
-    do aplicativo ou entre diferentes atividades.
+    do aplicativo ou entre diferentes atividades. Ele faz isso através da chave e valor;
+    
+    - Chave: Uma chave é uma string única que identifica um valor dentro do Bundle. É usada
+    como um identificador para recuperar o valor correspondente mais tarde. Cada chave em um
+    Bundle deve ser única, pois é usada para distinguir entre diferentes valores armazenados no Bundle.
+
+    - Valor: O valor associado a uma chave é o dado real que está sendo armazenado. Este pode ser de 
+    qualquer tipo de dado suportado pelo Bundle, incluindo primitivos como inteiros, strings, booleanos,
+    bem como objetos serializáveis (reconstruído a partir da sequência de bytes original) e parceláveis
+    (conversão do objeto em uma representação de bytes que pode ser transmitida ou armazenada).
      */
 
     /*
     --Super.OnCreate:
      O super.OnCreate(savedInstanceState) chama a implementação do método onCreate() da classe pai
-     (ComponentActivity) para garantir que o comportamento padrão seja executado antes de qualquer
+     (Activity) para garantir que o comportamento padrão seja executado antes de qualquer
      personalização adicional.
      */
 
@@ -47,6 +56,9 @@ class MainActivity : Activity() {
             Criado para substituir a ListView, é um componente que é usado para exibir vários dados de
             forma mais eficiente. Tem a característica de replicar todo o conteúdo até o final da tela.
             A função da RecyclerView aqui é exibir a lista de produtos na tela.
+            É uma boa prática utilizar o recycleview porque melhora o desempenho, flexibilidade e personalização,
+            suporte para diferentes layouts, entre outos benefícios, proporcionando uma experiência de usuário mais fluida e responsiva
+            nos aplicativos Android.
        */
 
         /*
